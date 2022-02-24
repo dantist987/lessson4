f0fd6149973e37ff37c62598716a633eb5fbb0bb
dantist987 <adik.isak2412@gmail.com>
2022-02-24 20:33:59 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,108 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+
+    static int bossHealth = 700;
+    static int bossDamage = 50;
+    static String bossDefenceType = "";
+    static int[] heroesHealth = {300, 320, 340, 360};
+    static int[] heroesDamage = {30, 35, 25, 20};
+    static String[] heroesAttackType = {"Physical", "Magical", "Kinetic", "Archer"};
+    static int roundCounter = 1;
+
+    public static void main(String[] args) {
+        while (isGameOver() != true) {
+            round();
+            roundCounter++;
+        }
+
+
+    }
+
+    public static void printStatistics() {
+        System.out.println("----------------");
+        System.out.println("Round: " + roundCounter);
+        System.out.println("Boss Health: " + bossHealth);
+        for (int i = 0; i < heroesAttackType.length; i++) {
+            System.out.println(heroesAttackType[i] + " health: " + heroesHealth[i]);
+        }
+        System.out.println("----------------");
+    }
+
+    public static void bossHits() {
+        for (int i = 0; i < heroesAttackType.length; i++) {
+            if (heroesHealth[i] > 0) {
+                if (heroesHealth[i] - bossDamage < 0) {
+                    heroesHealth[i] = 0;
+                } else {
+                    heroesHealth[i] = heroesHealth[i] - bossDamage;
+                }
+            }
+        }
+    }
+
+    public static void heroesHits() {
+        for (int i = 0; i < heroesAttackType.length; i++) {
+            if (heroesHealth[i] > 0 && bossHealth > 0) {
+                if (heroesAttackType[i] == bossDefenceType) {
+                    int randomDamage = new Random().nextInt(26);
+                    heroesDamage[i] = heroesDamage[i] - randomDamage;
+                    System.out.println(heroesAttackType[i] + " decrease damage on " + randomDamage);
+                } else {
+                    bossHealth = bossHealth - heroesDamage[i];
+                }
+            } else {
+                if (bossHealth - heroesDamage[i] < 0) {
+                    bossHealth = 0;
+                } else {
+                    bossHealth = bossHealth - heroesDamage[i];
+                }
+            }
+        }
+    }
+
+    public static boolean isGameOver() {
+        if (bossHealth <= 0) {
+            System.out.println("Heroes won!!!");
+            return true;
+        }
+
+        boolean allHeroesDead = true;
+        for (int i = 0; i < heroesAttackType.length; i++) {
+            if (heroesHealth[i] > 0) {
+                allHeroesDead = false;
+                break;
+            }
+        }
+
+        if (allHeroesDead == true) {
+            System.out.println("Boss won!!!");
+        }
+
+        return allHeroesDead;
+    }
+
+    public static void changeDefenceType() {
+        int randomAttackType = new Random().nextInt(heroesAttackType.length);
+        bossDefenceType = heroesAttackType[randomAttackType];
+        System.out.println("Boss choose: " + bossDefenceType);
+    }
+
+    public static void round() {
+        // Проверяем жив ли наш босс
+        if (bossHealth > 0) {
+            // Изменяем неуязвимость к определенному типу атаки
+            changeDefenceType();
+            // Наносит урон героям
+            bossHits();
+        }
+        // Герои наносят урон
+        heroesHits();
+        // Распечатка статистики
+        printStatistics();
+    }
+
+}
+
